91b83a989d1046a17ce8275f6fe0d1387a560be2
oOxianOo <240705427@qq.com>
2019-11-29 18:40:11 +0800

    修改redis的配置

--- flaskr/config.py
+++ flaskr/config.py
@@ -9,7 +9,7 @@ config_path = root_path.joinpath("config.json")
 env = {"ENV": "dev"}
 if config_path.exists():
     with open(str(config_path.resolve())) as fp:
-        env = json.load(fp)
+        env = {**env, **json.load(fp)}
 
 
 class ConfigBase:
@@ -17,10 +17,11 @@ class ConfigBase:
     SECRET_KEY = env.get("SECRET_KEY", 'YOU_SHOULD_SET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # Redis
-    REDIS_HOST = env.get("REDIS_HOST", 'http://localhost')
+    REDIS_HOST = env.get("REDIS_HOST", '127.0.0.1')
     REDIS_PORT = env.get("REDIS_PORT", 6379)
     REDIS_PASSWORD = env.get("REDIS_PASSWORD", None)
     REDIS_POOL_DB = env.get("REDIS_POOL_DB", 0)
+
 
 class DevelopmentConfig(ConfigBase):
     SQLALCHEMY_DATABASE_URI = env.get('DATABASE_URI', f"sqlite:///{root_path.joinpath('data-dev.db')}")
